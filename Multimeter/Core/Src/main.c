4f96d0f4f71c911eb60649a1a4bbcd4148b14a0f
SSEses16 <114469124+SSEses16@users.noreply.github.com>
2023-12-17 13:30:25 +0300

    Add ssd1306 lib

--- Multimeter/Core/Src/main.c
+++ Multimeter/Core/Src/main.c
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "ssd1306.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -92,7 +92,9 @@ int main(void)
   MX_I2C1_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  ssd1306_Init();
+  ssd1306_Fill(White);
+  ssd1306_UpdateScreen();
   /* USER CODE END 2 */
 
   /* Infinite loop */
